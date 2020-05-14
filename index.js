4bb106c80076a6067559ef63051c7573f252a1ac
Ruslan Efendiev <r.efendiev@bi.zone>
2020-05-14 19:27:56 +0300

    feat: added try catch block

--- index.js
+++ index.js
@@ -16,29 +16,32 @@ bot.on('inlineQuery', (msg) => {
     // Create a new answer list object
     const answers = bot.answerList(msg.id, { cacheTime: 60 });
 
-    await request(
-      `https://www.playphrase.me/api/v1/phrases/search?q=${escape(
-        replacedCharacterQuery
-      )}&limit=10&skip=0`,
-      { json: true },
-      (err, res, body) => {
-        if (err) {
-          return console.log(err);
-        }
-        body.phrases.forEach((phrase) => {
-          answers.addVideo({
-            id: phrase.id,
-            title: phrase['video-info'].info,
-            description: phrase.text,
-            thumb_url: 'https://avatarfiles.alphacoders.com/967/96712.jpg',
-            mime_type: 'video/mp4',
-            video_url: phrase['video-url'],
-            caption: phrase.text,
+    try {
+      await request(
+        `https://www.playphrase.me/api/v1/phrases/search?q=${escape(
+          replacedCharacterQuery
+        )}&limit=10&skip=0`,
+        { json: true },
+        (err, res, body) => {
+          if (err) {
+            return console.log(err);
+          }
+          body.phrases.forEach((phrase) => {
+            answers.addVideo({
+              id: phrase.id,
+              title: phrase['video-info'].info,
+              description: phrase.text,
+              thumb_url: 'https://avatarfiles.alphacoders.com/967/96712.jpg',
+              mime_type: 'video/mp4',
+              video_url: phrase['video-url'],
+              caption: phrase.text,
+            });
           });
-        });
-      }
-    );
-
+        }
+      );
+    } catch (e) {
+      console.log(e);
+    }
     // Send answers
     return bot.answerQuery(answers);
   };
